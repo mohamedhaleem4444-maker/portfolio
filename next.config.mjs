eb3a372b1a42f922240cccaf5f883c0fc2f60915
mohamedhaleem4444-maker <mohamedhaleem4444@gmail.com>
2026-08-03 10:53:27 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/Geomodels_Batch_Runner_Setup.exe',
-        headers: [
-          {
-            key: 'Content-Length',
-            value: '66566024',
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
